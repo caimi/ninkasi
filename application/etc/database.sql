532439c977ccd1ea24fb10bb53568d580a6faaaf
caimi <carlos.caimi@gmail.com>
2012-08-07 17:53:46 -0300

    database

--- application/etc/database.sql
+++ application/etc/database.sql
@@ -1,18 +1,33 @@
-CREATE TABLE IF NOT EXISTS `user` (
-  `id` int(6) NOT NULL AUTO_INCREMENT,
-  `username` varchar(32) NOT NULL,
-  `password` varchar(32) NOT NULL,
-  `id_person` int(11) DEFAULT NULL,
-  `creation` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `username` (`username`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+CREATE TABLE IF NOT EXISTS `activation` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `code` varchar(64) NOT NULL,
+  `status` varchar(2) NOT NULL,
+  `id_user` int(11) DEFAULT NULL,
+  `type` varchar(2) NOT NULL,
+  `creation` datetime NOT NULL,
+  `conclusion` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
 
 CREATE TABLE IF NOT EXISTS `person` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `surname` varchar(50) NOT NULL,
   `lastname` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `creation` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `creation` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=17 ;
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(6) NOT NULL AUTO_INCREMENT,
+  `username` varchar(32) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `id_person` int(11) DEFAULT NULL,
+  `activation` datetime DEFAULT NULL,
+  `creation` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=18 ;
+
+INSERT INTO `user` (`id`, `username`, `password`, `id_person`, `activation`, `creation`) VALUES
+(1, 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', NULL, '2012-08-06 13:38:37', '2012-08-06 16:38:37');
