cd2fd686d544fa1e710aab34f74c321b1bf40233
caimi <carlos.caimi@gmail.com>
2012-08-13 17:42:59 -0300

    layout

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -3,16 +3,33 @@
 		<ul>
 			<li><a href=""><img src="<?php echo base_url();?>includes/ui-perfil.png" class="ui-perfil"></a><span>perfil</span></li>
 			<li><a href=""><img src="<?php echo base_url();?>includes/ui-perfil.png" class="ui-perfil"></a><span>receitas</span></li>
-			<li><a href=""><img src="<?php echo base_url();?>includes/ui-perfil.png" class="ui-perfil"></a><img src=""><span>favoritas</span></li>
-			<li><a href=""><img src="<?php echo base_url();?>includes/ui-perfil.png" class="ui-perfil"></a><img src=""><span>insumos</span></li>
-			<li><a href=""><img src="<?php echo base_url();?>includes/ui-perfil.png" class="ui-perfil"></a><img src=""><span>estilos</span></li>
-			<li><a href=""><img src="<?php echo base_url();?>includes/ui-perfil.png" class="ui-perfil"></a><img src=""><span>instrumentos</span></li>
-			<li><a href=""><img src="<?php echo base_url();?>includes/ui-perfil.png" class="ui-perfil"></a><img src=""><span>conversões</span></li>
-			<li><a href=""><img src="<?php echo base_url();?>includes/ui-perfil.png" class="ui-perfil"></a><img src=""><span>mensagens</span></li>
+			<li><a href=""><img src="<?php echo base_url();?>includes/ui-perfil.png" class="ui-perfil"></a><span>insumos</span></li>
+			<li><a href=""><img src="<?php echo base_url();?>includes/ui-perfil.png" class="ui-perfil"></a><span>estilos</span></li>
+			<li><a href=""><img src="<?php echo base_url();?>includes/ui-perfil.png" class="ui-perfil"></a><span>instrumentos</span></li>
+			<li><a href=""><img src="<?php echo base_url();?>includes/ui-perfil.png" class="ui-perfil"></a><span>conversões</span></li>
+			<li><a href=""><img src="<?php echo base_url();?>includes/ui-perfil.png" class="ui-perfil"></a><span>mensagens</span></li>
 		</ul>
 	</div>
 	<div id="secBar">
-		todos
+		<div style="height: 64px; border-bottom: 1px solid #999999">Perfil</div>
+		<div style="height: 28px">
+			<ul class="tabbed">
+				<li class="ativo"><a href=""><img class="icon-receita"></img></a></li>
+				<li><a href=""><img class="icon-filtro"></img></a></li>
+				<li><a href=""><img class="icon-pessoa"></img></a></li>
+			</ul>
+			<ul class="button">
+				<li class="ativo"><a href="">Minhas Receitas</a></li>
+				<li><a href="">Receitas Compartilhadas</a></li>
+				<li><a href="">Favoritas</a></li>
+				<li><a href="">Calendário</a></li>
+			</ul>
+		</div>
 	</div>
 </div>
-<div id="main">put your recept</div>
+<div id="main">
+	<div style="height: 64px; border-bottom: 1px solid #999999">titulo</div>
+	<div style="height: 28px; border-bottom: 1px solid #999999">barra
+		<p class="icon-receita"></p>	
+	</div>
+</div>
