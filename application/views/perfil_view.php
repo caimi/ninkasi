4c31adac69db4c13c10a6a032acea0331f6964c4
caimi <carlos.caimi@gmail.com>
2012-08-29 19:07:46 -0300

    interface

--- application/views/perfil_view.php
+++ application/views/perfil_view.php
@@ -11,15 +11,20 @@
 		</ul>
 		<hr width="95%">
 		<div id="#tab1">
-			<div class="wrap">
-				aqui vai o detalhe 1<br>
-				<input type="button" name="enviar" value="enviar">
+			<div>
+				<div class="button">
+					<ul>
+						<li>Dados pessoais</li>
+						<li>Sites</li>
+						<li>Redes</li>
+					</ul>
+				</div>
 			</div>
 		</div>
 		<div id="#tab2">
 			<div class="wrap">
 				aqui vai o detalhe 2<br>
-				<a href="#" class="ui-button">enviar</a>
+				<a href="#" class="ui-link-button">enviar</a>
 			</div>
 		</div>
 		<div id="#tab3">
@@ -31,18 +36,41 @@
 </div>
 <div style="background-color: #e7e7e7; margin-left: 325px;">
 	<div style="background-color: #e7e7e7; height: 64px; border: 1px solid #a1a1a1">
-		<span style="display: inline-block; padding: 5px; color: #FFAE00; margin: 15px;" class="ui-fnt-big">Perfil</span>
+		<span style="display: inline-block; padding: 5px; color: #843121; margin: 15px;" class="ui-fnt-big">Perfil</span>
 		<div style="float: right; height: 64px; width: 96px;padding-top: 16px; text-align: center"><a href="#">12</a><br>receitas</div>
 		<div style="float: right; height: 64px; width: 96px;padding-top: 16px; text-align: center"><a href="#">4</a><br>mensagens</div>
 		<div style="float: right; height: 64px; width: 96px;padding-top: 16px; text-align: center"><a href="#">10</a><br>visitas</div>
 	</div>
-	<div style="background-color: #e7e7e7; height: 32px; border-bottom: 1px solid #a1a1a1">
-	</div>
-	<div style="display: table;">
-		<div style="padding:10px; float: left; width: 800px;">Perfil e preferências<br>
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	<div style="height: 32px; border-bottom: 1px solid #a1a1a1">
+		<div style="padding-left: 10px; font-size: .7em; font-family: Helvetica; color: #666666; padding-top: 7px;">perfil > configurações
 		</div>
-		<div style="float: right; background-color: #dfdfdf; width: 225px; height: 600px; display: table">propaganda</div>	
+	</div>
+	<div style="display:table;">
+		<div style="padding:10px; float: left; display: inline-block; width: 80%">Perfil e preferências<br>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eget nisl velit, id vulputate nunc. Donec pulvinar dignissim metus, eget iaculis sapien imperdiet vel. Phasellus rhoncus euismod velit, in pulvinar libero sodales eleifend. Sed sagittis neque eget erat faucibus et imperdiet ante dignissim. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam dapibus imperdiet mi ac semper. In hac habitasse platea dictumst. In at risus tortor, ut suscipit tortor. Nunc in gravida augue. In nibh lorem, bibendum non laoreet non, ornare eget enim.
+			</p>
+			<p>
+				Mauris et diam justo, nec posuere erat. Nam id ipsum in leo posuere aliquam a aliquet metus. Fusce a vestibulum dolor. Vestibulum venenatis nisi in dui gravida quis ullamcorper magna venenatis. Aliquam adipiscing, nibh facilisis cursus imperdiet, justo sem imperdiet dolor, vel imperdiet sem odio a sem. Curabitur nunc justo, vulputate a vestibulum ac, pharetra imperdiet sapien. Etiam at augue mauris. Praesent est arcu, commodo vitae adipiscing semper, suscipit in turpis. Praesent rhoncus malesuada tortor. Nam ultricies dui non magna sodales consequat eget ornare tellus. Etiam ac blandit lectus. Integer sed nisi leo, sed dictum risus. Vivamus vitae nisi ante, quis condimentum risus. Nunc et lorem ligula, et varius metus. Donec aliquam augue id eros ultrices ultricies.
+			</p>
+		</div>
+		<div style="float: right; min-height: 600px;">
+			<div style="padding:5px; background-color: #ffffff; border: 1px solid #a1a1a1; border-radius: 3px; margin:5px; font-family: helvetica; font-size: 10px;">
+				<span style="padding: 0px 0; display: inline-block">Publicidade</span>
+				<div style="width: 200px; height: 125px; border: 1px solid #a1a1a1; background-color: #e7e7e7; text-align: center; margin: 10px 0">
+					<span style="font-family: Helvetica; font-size: 18pt; color: #a1a1a1; font-weight: bold; margin-top: 30px; display: block">200 x 125</span><br>
+					<span style="font-family: Helvetica; font-size: 10pt; color: #a1a1a1">Anuncie aqui?</span>
+				</div>
+				<div style="width: 200px; height: 125px; border: 1px solid #a1a1a1; background-color: #e7e7e7; text-align: center; margin: 10px 0">
+					<span style="font-family: Helvetica; font-size: 18pt; color: #a1a1a1; font-weight: bold; margin-top: 30px; display: block">200 x 125</span><br>
+					<span style="font-family: Helvetica; font-size: 10pt; color: #a1a1a1">Anuncie aqui?</span>
+				</div>
+				<div style="width: 200px; height: 125px; border: 1px solid #a1a1a1; background-color: #e7e7e7; text-align: center; margin: 10px 0">
+					<span style="font-family: Helvetica; font-size: 18pt; color: #a1a1a1; font-weight: bold; margin-top: 30px; display: block">200 x 125</span><br>
+					<span style="font-family: Helvetica; font-size: 10pt; color: #a1a1a1">Anuncie aqui?</span>
+				</div>
+			</div>
+		</div>	
 	</div>
 </div>
 
